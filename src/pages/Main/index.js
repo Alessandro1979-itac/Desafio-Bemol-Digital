05d49f7f9696983978387f81af1c93f039bb7b83
Alessandro <sandromuniz27@hotmail.com>
2020-07-23 14:22:15 -0400

    Adicionado a página inicial do projeto

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 function Main() {
@@ -6,9 +7,7 @@ function Main() {
     <div className="container">
       <h1>Bem vindo a página de Cadastro de Clientes</h1>
       <p>Se ainda não possui cadastro clique no botão abaixo</p>
-      <button>
-        <a href="../Cadastro">Cadastrar</a>
-      </button>
+      <Link to='/cadastro' className="link-back link-back--hover"><button>Cadastrar</button></Link>
     </div>
   );
 }
